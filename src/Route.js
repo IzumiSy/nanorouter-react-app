64d46816a158ea6dc6a123efe07fa6d0f92dec7e
IzumiSy <beetle-noise@gmx.com>
2020-03-21 18:44:47 +0900

    useMemoを使う

--- src/Route.js
+++ src/Route.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import nanorouter  from "nanorouter"
 
+const useRouter = (routes) => {
+  const _router = (routes, initialPath) => React.useMemo(() => {
+    const router = nanorouter({ default: '/' })
+    let nextComponent = null
+
+    Object.keys(routes).forEach(path => {
+      router.on(path, param => { nextComponent = routes[path] })
+    })
+
+    router.emit(initialPath)
+
+    return nextComponent
+  }, [routes, initialPath])
+
+  return initialPath => _router(routes, initialPath)
+}
+
 export const Route = routes => {
-  const router = nanorouter({ default: '/' })
-  let nextComponent = null
-
-  Object.keys(routes).forEach(path => {
-    router.on(path, param => { nextComponent = routes[path] })
-  })
-
-  router.emit(window.location.pathname)
+  const router = useRouter(routes)
+  const nextComponent = router(window.location.pathname)
 
   return (
     <div className="container">
